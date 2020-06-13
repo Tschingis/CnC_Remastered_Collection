68cccd914fc4f28298f90a8cc947c0308a513614
Tschingis <tschingis-chan@gmx.de>
2020-06-13 18:11:59 +0200

    -removed diagonal seperation from ccgraphmap

--- TIBERIANDAWN/CCGraphMap.cpp
+++ TIBERIANDAWN/CCGraphMap.cpp
@@ -30,35 +30,15 @@ int CCGraphMap::getEdges(Edge edges[MAX_EDGES], int nodeId) const
 {
 	int numEdges = 0;
 	int threat = 0;
-	int straight[4] = { 0, 2, 4, 6 };
-	int diagonal[4] = { 1, 3, 5, 7 };
-	unsigned char walkable[8];
-	memset(walkable, 0, sizeof(walkable));
 
 	// straight movements, which can be issued with no problems
-	for (int i = 0; i < 4; ++i) {
-		FacingType dir = (FacingType)straight[i];
+	for (int i = 0; i < FACING_COUNT; ++i) {
+		FacingType dir = (FacingType)i;
 		CELL next = Adjacent_Cell((CELL)nodeId, dir);
 
 		auto cost = mMoveable->Passable_Cell(next, dir, threat, mThreshhold);
 		if (cost) {
-			walkable[dir] = 1;
-			edges[numEdges].cost = cost*2;
-			edges[numEdges].targetNodeId = next;
-			++numEdges;
-		}
-	}
-	// diagonal movements, which can only be issued if left AND right neighbors are walkable
-	for (int i = 0; i < 4; ++i) {
-		FacingType dir = (FacingType)diagonal[i];
-		if (!(walkable[dir - 1] && walkable[dir + 1 % 8]))
-			continue;
-
-		CELL next = Adjacent_Cell((CELL)nodeId, dir);
-
-		auto cost = mMoveable->Passable_Cell(next, dir, threat, mThreshhold);
-		if (cost) {
-			edges[numEdges].cost = cost * 3;
+			edges[numEdges].cost = i % 2 ? cost * 3 : cost * 2; // if even -> straight movement, uneven -> diagonal movement
 			edges[numEdges].targetNodeId = next;
 			++numEdges;
 		}
